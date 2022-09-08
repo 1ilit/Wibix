9c9bd820e1ad9eaf225c13e32cf2a8b710d85c74
daywalker <lilitmousheghyan@gmail.com>
2022-09-08 12:38:49 +0300

    day 17

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,29 @@
-import React, {useRef, useEffect, useState}  from "react";
+import React, { useRef, useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
 import HomeBanner from "../components/HomeBanner";
 import Footer from "../components/Footer";
-import {motion} from 'framer-motion'
+import { motion } from "framer-motion";
 import sliderImages from "../sliderimages";
+// import { urlRes } from "../endpoints";
+// import axios from "axios";
+import { Link } from "react-router-dom";
 const Home = (props) => {
-  const [width, setWidth]=useState(0);
-  const slider=useRef();
-  useEffect(()=>{
-    setWidth(slider.current.scrollWidth-slider.current.offsetWidth);
-  }, [])
+  const [width, setWidth] = useState(0);
+  const slider = useRef();
+
+  useEffect(() => {
+    setWidth(slider.current.scrollWidth - slider.current.offsetWidth);
+  }, []);
+
   return (
     <>
       <Navbar data={props.data} color="white" />
       <HomeBanner />
       <div className="bg-grey m-0 p-2">
-        <motion.h2 className="fw-bold text-center my-4">
+        <motion.h2 className="fw-bold text-center my-5">
           Start your journey on <span className="logo-font">wibix</span>
         </motion.h2>
-        <div className="v-card bg-white mt-3 mx-5 px-4 py-5 px-md-14 py-md-6 mx-md-12 elevation-4 border border-secondary shadow-lg p-3 mb-5 bg-white rounded">
+        <div className="v-card bg-white mt-3 mx-5 px-4 py-5 px-md-14 py-md-6 mx-md-12 elevation-4 border border-secondary shadow-lg p-3 my-6 bg-white rounded">
           <div className="row justify-space-around flex-row px-2">
             <div className="px-0 col-md-4 col-12">
               <div className="d-flex justify-space-around align-center pb-4 px-7 px-md-0 pl-md-8">
@@ -101,22 +106,57 @@ const Home = (props) => {
           </div>
         </div>
 
-        <div className="my-5">
-          <h2 className="fw-bold text-center my-4">Connect with people from universities all over the world</h2>
+        <div className="my-6">
+          <h2 className="fw-bold text-center my-4">
+            Connect with people from universities all over the world
+          </h2>
           <motion.div ref={slider} className="slider">
-            <motion.div drag="x" dragConstraints={{right: 0, left:-width}} className="inner-slider">
-              {
-                sliderImages.map((v, i)=>{
-                  return(
-                    <motion.div key={i} className="item">
-                      <img src={v} alt="slider-img"/>
-                    </motion.div>
-                  )
-                })
-              }
+            <motion.div
+              drag="x"
+              dragConstraints={{ right: 0, left: -width }}
+              className="inner-slider"
+            >
+              {sliderImages.map((v, i) => {
+                return (
+                  <motion.div key={i} className="item">
+                    <img src={v} alt="slider-img" />
+                  </motion.div>
+                );
+              })}
             </motion.div>
           </motion.div>
         </div>
+
+        <hr/>
+
+        <div className="row mt-5 mb-4 mx-0">
+          
+            <div className="col-md-6 text-end">
+              <img
+                src="https://cdn-icons-png.flaticon.com/512/3846/3846889.png"
+                width="200"
+                alt="folder pic"
+                className="me-3"
+              />
+            </div>
+
+            <div className="col-md-6 text-start">
+              <div className="mb-5 ms-3">
+                <h5 className="fw-bold">Become One of Us!</h5>
+                <p className="my-3 mb-4 w-md-50">
+                  Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, veritatis quaerat temporibus accusantium suscipit eaque quisquam blanditiis sunt accusamus dolorem optio quo impedit excepturi quod exercitationem error harum eum facilis!
+                </p>
+                <h5 className="mt-3">
+                  <Link
+                    to="/signup"
+                    className="btn-burnt-umber px-3 py-2"
+                  >
+                    Get Started
+                  </Link>
+                </h5>
+              </div>
+            </div>
+          </div>
       </div>
       <Footer />
     </>
